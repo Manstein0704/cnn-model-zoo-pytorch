a1d24a9e5676e79332e706d4159c577196283d6f
城戸 洸之介 <kounosuke0704@icloud.com>
2026-08-21 15:23:36 +0900

    modified alexnet.py

--- models/alexnet.py
+++ models/alexnet.py
@@ -8,21 +8,22 @@ import torch.nn as nn
 #AlexNet Scratch
 ####################
 
+#※Adjust the first convolutional layer's kernel size and stride for CIFAR-10.
 
 class AlexNet(nn.Module):
     def __init__(self, num_classes:int):
         super().__init__()
         self.net = nn.Sequential(
-            nn.LazyConv2d(96, kernel_size=11, stride=4, padding=1),
+            nn.LazyConv2d(96, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.LazyConv2d(256, kernel_size=5, stride=2),
+            nn.LazyConv2d(256, kernel_size=5, padding=2),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
             nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
             nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, padding=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Flatten(),
             nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(p=0.5),
             nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(p=0.5),
