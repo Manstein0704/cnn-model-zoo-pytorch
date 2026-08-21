49a31be520708465bf45d798ecec2a793af16eca
城戸 洸之介 <kounosuke0704@icloud.com>
2026-08-21 15:08:08 +0900

    modified alexnet.py

--- models/alexnet.py
+++ models/alexnet.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 
 ####################
-#AlexNet
+#AlexNet Scratch
 ####################
 
 
@@ -24,9 +24,9 @@ class AlexNet(nn.Module):
             nn.LazyConv2d(384, kernel_size=3, padding=1), nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, padding=2),
             nn.Flatten(),
-            nn.Linear(4096), nn.ReLU(), nn.Dropout(p=0.5),
-            nn.Linear(4096), nn.ReLU(), nn.Dropout(p=0.5),
-            nn.Linear(num_classes)
+            nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(p=0.5),
+            nn.LazyLinear(4096), nn.ReLU(), nn.Dropout(p=0.5),
+            nn.LazyLinear(num_classes)
         )
 
     def forward(self, X):
